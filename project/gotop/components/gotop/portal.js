234f2d022eb0d8d4e92a51823f2f4461aa2807d1
张意立(媒体产品技术中心) <yilizhang@sohu-inc.com>
2014-12-16 03:44:23 +0800

    增加火箭

--- project/gotop/components/gotop/portal.js
+++ project/gotop/components/gotop/portal.js
@@ -1,11 +1,23 @@
-define(['jquery'], function ($) {
+;define(['jquery'], function ($) {
     'use strict';
     //初始化组件类，参数为组件配置，如果组件第一次创建，将传递空配置，如果组件为已经创建到视图窗口，重新加载，将传递已保存的配置
     return {
         //输出到发布页面，当用户正式发布后，调用此函数创建视图。
         onAfterRender: function (el) {
-            $(el).find('.helloworld').html('helloworld');
-            alert(1);
+            for(var i =0; i<10; i++){
+                $('body').append($('<div style="height:200px;border-bottom:1px solid #333;"></div>'));
+            }
+            $(el).on(
+                'touchstart',function(){
+                    var $self = $(this);
+                    $self.addClass('onsoso');
+                    $('html,body').animate({
+                        scrollTop: 0
+                    }, 500,function(){
+                        $self.removeClass('onsoso');
+                    });
+                }
+            );
         }
     }
 });
